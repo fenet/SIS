3d00ad4998caccdcd0e975282802473144788950
fenet <fenetgucci@gmail.com>
2023-07-02 17:11:13 +0300

    yic

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,9 +4,9 @@ ActiveAdmin.register_page "Dashboard" do
   content title: proc { I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
-        div image_tag("banner.jpg", size: "250x150")
-        span "Welcome To Hope Enterprise University College"
-        small "This is Hope Enterprise University College registrar and school management portal system."
+        div image_tag("church.jpg", size: "250x150")
+        span "Welcome To Yardstick International College"
+        small "This is Yardstick International College's registrar and school management portal system."
       end
     end
 
