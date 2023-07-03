a7152cbd898f7873d8f4ec5c7476d2d476f190eb
fenet <fenetgucci@gmail.com>
2023-07-03 16:58:55 +0300

    sshtry

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bund
 set :keep_releases, 5
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
-# set :git_ssh_command, "ssh -o ForwardAgent=yes"
+set :git_ssh_command, "ssh -o ForwardAgent=yes"
 
 # set :ssh_options, { :forward_agent => true }
 # Default branch is :master
