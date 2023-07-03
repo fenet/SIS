5ac138c0ed494855420f1630b176fbdc634c3180
fenet <fenetgucci@gmail.com>
2023-07-03 16:03:45 +0300

    sshagent

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,9 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bund
 set :keep_releases, 5
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
-set :ssh_options, { :forward_agent => true }
+set :git_ssh_command, "ssh -o ForwardAgent=yes"
+
+# set :ssh_options, { :forward_agent => true }
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
