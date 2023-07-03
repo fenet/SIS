9aebefdf1d79ed46a8385b53a43f4f614d079363
fenet <fenetgucci@gmail.com>
2023-07-03 08:48:41 +0300

    capdeployssh

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,6 +15,8 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bund
 
 set :keep_releases, 5
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
+
+ssh_options[:forward_agent] = true
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
