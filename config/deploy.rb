ded52400a43f2a3718521392214192b506f2fda9
fenet <fenetgucci@gmail.com>
2023-07-04 19:14:26 +0300

    lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # config valid for current version and patch releases of Capistrano
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.3"
+lock "~> 3.16.0"
 
 set :application, "yic_production"
 set :repo_url, "git@github.com:fenet/yic_portal.git"
