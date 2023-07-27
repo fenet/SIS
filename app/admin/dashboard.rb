bfaa53d48599ec3b70f55c62032db048f492510b
fenet <fenetgucci@gmail.com>
2023-07-27 14:33:17 +0300

    firsttry

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,9 +4,9 @@ ActiveAdmin.register_page "Dashboard" do
   content title: proc { I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
-        div image_tag("logo.jpg", size: "250x150")
-        span "Welcome To Hope Enterprise University College"
-        small "This is Hope Enterprise University College's registrar and school management portal system."
+        div image_tag("leadstar.jpg", size: "250x150")
+        span "Welcome To Leadstar College"
+        small "This is Leadstar College's registrar and school management portal system."
       end
     end
 
