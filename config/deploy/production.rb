5234c5725208075fb591237aa9e52b18d918fe39
fenet <fenetgucci@gmail.com>
2023-09-22 14:45:40 +0300

    ngvcdeploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ role :app, %w{newdeploy@141.95.85.206}
 role :web, %w{newdeploy@141.95.85.206}
 role :db, %w{newdeploy@141.95.85.206}, :primary => true
 
-set :branch, "main"
+set :branch, "ngvcnew"
 set :rails_env, "production"
 
 set :deploy_to, '/home/newdeploy/ngvcnew' 
