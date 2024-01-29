d9077602afe16a4842cec62e3a7ee7ac962e5d86
fenet <fenetgucci@gmail.com>
2024-01-29 19:15:32 +0300

    paymenttranupdate

--- app/models/payment_transaction.rb
+++ app/models/payment_transaction.rb
@@ -1,10 +1,10 @@
 class PaymentTransaction < ApplicationRecord
 	after_create :set_invoice_status
 	##validations
-    validates :account_holder_fullname , :presence => true, :length => { :within => 2..50 }, format: { with: /\A[a-zA-Z]+\z/, message: "Only letters are allowed" }
-    validates :transaction_reference , :presence => true, :length => { :within => 2..50 }
-	validates :phone_number, :presence => true, format: { with: /\A[1-9]\d*\z/, message: "Only positive numbers are allowed" }
-	validates :account_number, :presence => true, format: { with: /\A[1-9]\d*\z/, message: "Only positive numbers are allowed" }
+    #validates :account_holder_fullname , :presence => true, :length => { :within => 2..50 }, format: { with: /\A[a-zA-Z]+\z/, message: "Only letters are allowed" }
+    #validates :transaction_reference , :presence => true, :length => { :within => 2..50 }
+	#validates :phone_number, :presence => true, format: { with: /\A[1-9]\d*\z/, message: "Only positive numbers are allowed" }
+	#validates :account_number, :presence => true, format: { with: /\A[1-9]\d*\z/, message: "Only positive numbers are allowed" }
 	##associations
   	# belongs_to :invoice
     belongs_to :invoiceable, polymorphic: true
