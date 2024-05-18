9b021e6034d8cdbdb9e4acca8ee6af56270f463e
melashu <meshu102@outlook.com>
2024-05-18 23:13:22 +0300

    Allow student to see there grade

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,18 @@
 class PagesController < ApplicationController
-  before_action :authenticate_student!, only: [:enrollement, :documents, :profile, :dashboard, :create_semester_registration]
+  before_action :authenticate_student!,
+                only: %i[enrollement documents profile dashboard create_semester_registration]
   # layout false, only: [:home]
   def home
     # authenticate_student!
   end
 
-  def admission
-  end
+  def admission; end
 
-  def documents
-  end
+  def documents; end
 
-  def digital_iteracy_quiz
-  end
+  def digital_iteracy_quiz; end
 
-  def requirement
-  end
+  def requirement; end
 
   def profile
     @address = current_student.student_address
@@ -26,8 +23,12 @@ class PagesController < ApplicationController
     @address = current_student.student_address
     @emergency_contact = current_student.emergency_contact
     @invoice = Invoice.find_by(student: current_student, semester: current_student.semester, year: current_student.year)
-    @smr = current_student.semester_registrations.where(year: current_student.year, semester: current_student.semester).last
-    @payment_remaining = current_student.semester_registrations.where("remaining_amount > ?", 0).last if @smr.nil?
+    @smr = current_student.semester_registrations.where(year: current_student.year,
+                                                        semester: current_student.semester).last
+    @payment_remaining = current_student.semester_registrations.where('remaining_amount > ?', 0).last if @smr.nil?
+    @student_grades = StudentGrade.eager_load(:course_registration).where('course_registrations.year=?', current_student.year).where(
+      'course_registrations.year=?', current_student.semester
+    ).where(student: current_student).includes(:course)
   end
 
   def enrollement
@@ -44,17 +45,19 @@ class PagesController < ApplicationController
 
   def create_semester_registration
     mode_of_payment = params[:mode_of_payment]
-    if params[:out_of_batch].present?
-      registration = current_student.add_student_registration(mode_of_payment, true)
-    else
-      registration = current_student.add_student_registration(mode_of_payment, false)
-    end
+    registration = if params[:out_of_batch].present?
+                     current_student.add_student_registration(mode_of_payment, true)
+                   else
+                     current_student.add_student_registration(mode_of_payment, false)
+                   end
     respond_to do |format|
       if registration.save
-        format.html { redirect_to invoice_path(registration.invoices.last.id), notice: "Registration was successfully created." }
+        format.html do
+          redirect_to invoice_path(registration.invoices.last.id), notice: 'Registration was successfully created.'
+        end
         format.json { render :show, status: :ok, location: registration }
       else
-        format.html { redirect_to :enrollement_path, alert: "Something went wrong please try again" }
+        format.html { redirect_to :enrollement_path, alert: 'Something went wrong please try again' }
         format.json { render json: registration.errors, status: :unprocessable_entity }
       end
     end
